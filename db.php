7da18ec02a41132f33e2c12298b37ddeaa42d28b
staybreeze <wunshengliao@gmail.com>
2023-11-17 14:25:16 +0800

    feat:新增自訂函式以取得資料表的資料

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+
+$rows=all('dept');
+
+dd($rows);
+
+function all($table){
+
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');
+
+if(isset($table)&& !empty($table)){
+$sql="select * from `$table`";
+$rows=$pdo->query($sql)->fetchAll();
+return $rows;
+}else{
+    echo "錯誤:沒有指定的資料表名稱";
+}
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
