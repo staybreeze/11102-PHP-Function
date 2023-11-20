609441a3206a05213ae731f05336f458da0adeb9
staybreeze <wunshengliao@gmail.com>
2023-11-20 10:52:28 +0800

    feat:新增insert()自訂函式及試作del()自訂函式

--- db_01.php
+++ db_01.php
@@ -10,9 +10,16 @@
 //echo "<h3>相同條件使用all()</h3>";
 //dd($rows);
 
-$up = update("students", '3', ['dept' => '16', 'name' => '張明珠']);
+// $up = update("students", '3', ['dept' => '16', 'name' => '張明珠']);
 
-dd($up);
+// dd($up);
+// $row = del('students', 2);
+insert('dept',['code'=>'170','name'=>'戲劇系']);
+
+
+// -----all-----
+
+// SELECT `col1`,`col2`,... FROM `table1`,`table2`,...　WHERE ...
 function all($table = null, $where = '', $other = '')
 {
     $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
@@ -46,6 +53,7 @@ function all($table = null, $where = '', $other = '')
     }
 }
 
+// -----find-----
 
 function find($table, $id)
 {
@@ -68,13 +76,18 @@ function find($table, $id)
     return $row;
 }
 
+// -----update-----
+
+// UPDATE `table` SET `col1`='value1',`col2`='value2',...　WHERE ...
+
 function update($table, $id, $cols)
 {
     $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     $pdo = new PDO($dsn, 'root', '');
 
     $sql = "update `$table` set ";
-
+    // 因為要填入兩個變數，因此要分別判斷兩個變數
+    // 判斷$cols
     if (!empty($cols)) {
         foreach ($cols as $col => $value) {
             $tmp[] = "`$col`='$value'";
@@ -85,6 +98,21 @@ function update($table, $id, $cols)
 
     $sql .= join(",", $tmp);
 
+    // 判斷$id
+
+    // 當$id只要查id的時候
+    // if (is_array($id)) {
+    //     foreach ($id as $col => $value) {
+    //         $tmp[] = "`$col`='$value'";
+    //     }
+    // } elseif (is_numeric($id)) {
+    //     $sql .= " where `id`='$id'";
+    // } else {
+    //     echo "錯誤:參數的資料型態應為數字或陣列";
+    // }
+
+
+    // $id可以不只查id，也可以查其他的where
     if (is_array($id)) {
         foreach ($id as $col => $value) {
             $tmp[] = "`$col`='$value'";
@@ -97,6 +125,64 @@ function update($table, $id, $cols)
     }
     echo $sql;
     return $pdo->exec($sql);
+
+}
+
+// -----delete-----
+
+// DELETE FROM `table` WHERE ...
+
+function del($table, $id)
+{
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo = new PDO($dsn, 'root', '');
+    $sql = "delete from `$table` ";
+
+    if (is_array($id)) {
+        foreach ($id as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        $sql .= " where " . join(" && ", $tmp);
+    } else if (is_numeric($id)) {
+        $sql .= " where `id`='$id'";
+    } else {
+        echo "錯誤:參數的資料型態比須是數字或陣列";
+    }
+
+    echo 'del=>' . $sql;
+    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    return $row;
+}
+
+// -----insert-----
+
+
+// INSERT INTO `table`(`col1`,`col2`,`col3`,`col4`,`col5`) 
+//             VALUES('value1','value1','value1','value1','value1','value1');
+
+
+
+function insert($table,$values){
+
+    
+$dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+$pdo = new PDO($dsn, 'root', '');
+
+$sql = "insert into `$table` ";
+
+// $cols="(``,``,``,``,)";
+// $vals="('','','','',)";
+
+$cols="(`".join("`,`",array_keys($values))."`)";
+$vals="('".join("','",$values)."')";
+
+$sql=$sql . $cols  ." values ".$vals;
+// $sql=insert into `$table` . (``,``,``,``,) ." values ".('','','','',);
+
+echo $sql;
+return $pdo->exec($sql);
+
+
 }
 
 function dd($array)
